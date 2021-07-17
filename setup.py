9eac3c7ac2618e1da331b6e92c801c1faca85cca
Viktor Vasilyev <an.viktory@gmail.com>
2021-07-17 22:36:33 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='AlgoTrader',
+   version='0.0.1',
+   description='AlgoTrader',
+   author='Viktor Vasilyev',
+   author_email='an.viktory@gmail.com',
+   packages=['AlgoTrader'],
+   install_requires=['pandas', 'numpy', 'matplotlib'],
+)
