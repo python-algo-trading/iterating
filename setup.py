d1a26ae57777a2a676728939ff44c8a2f04f5dd2
Viktor Vasilyev <an.viktory@gmail.com>
2021-07-23 13:14:42 +0200

    fix: add module for export

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='AlgoTrader',
-   version='0.0.1',
+   version='0.0.3',
    description='AlgoTrader',
    author='Viktor Vasilyev',
    author_email='an.viktory@gmail.com',
